567c31a645d8def659325cf72007a01fbc37cacc
Amit Dey <amdey@microsoft.com>
2022-01-27 10:49:01 -0800

    replace map with string

--- src/app.js
+++ src/app.js
@@ -3,30 +3,28 @@
  * Licensed under the MIT License.
  */
 
-import { SharedMap } from "fluid-framework";
+import { SharedString } from "fluid-framework";
 import { TinyliciousClient } from "@fluidframework/tinylicious-client";
-
-export const diceValueKey = "dice-value-key";
 
 // Load container and render the app
 
 const client = new TinyliciousClient();
 const containerSchema = {
-    initialObjects: { diceMap: SharedMap }
+    initialObjects: {diceString : SharedString}
 };
 const root = document.getElementById("content");
 
 const createNewDice = async () => {
     const { container } = await client.createContainer(containerSchema);
-    container.initialObjects.diceMap.set(diceValueKey, 1);
+    container.initialObjects.diceString.insertText(0, "Hello World!");
     const id = await container.attach();
-    renderDiceRoller(container.initialObjects.diceMap, root);
+    renderDiceRoller(container.initialObjects.diceString, root);
     return id;
 }
 
 const loadExistingDice = async (id) => {
     const { container } = await client.getContainer(id, containerSchema);
-    renderDiceRoller(container.initialObjects.diceMap, root);
+    renderDiceRoller(container.initialObjects.diceString, root);
 }
 
 async function start() {
@@ -48,8 +46,6 @@ const template = document.createElement("template");
 template.innerHTML = `
   <style>
     .wrapper { text-align: center }
-    .dice { font-size: 200px }
-    .roll { font-size: 50px;}
   </style>
   <div class="wrapper">
     <div class="dice"></div>
@@ -57,24 +53,21 @@ template.innerHTML = `
   </div>
 `
 
-const renderDiceRoller = (diceMap, elem) => {
+const renderDiceRoller = (diceString, elem) => {
     elem.appendChild(template.content.cloneNode(true));
 
     const rollButton = elem.querySelector(".roll");
     const dice = elem.querySelector(".dice");
 
-    // Set the value at our dataKey with a random number between 1 and 6.
-    rollButton.onclick = () => diceMap.set(diceValueKey, Math.floor(Math.random() * 6) + 1);
+    rollButton.onclick = () => diceString.insertText(0, "A",);
 
     // Get the current value of the shared data to update the view whenever it changes.
     const updateDice = () => {
-        const diceValue = diceMap.get(diceValueKey);
-        // Unicode 0x2680-0x2685 are the sides of a dice (⚀⚁⚂⚃⚄⚅)
-        dice.textContent = String.fromCodePoint(0x267f + diceValue);
-        dice.style.color = `hsl(${diceValue * 60}, 70%, 30%)`;
+        const diceValue = diceString.getText();
+        dice.textContent = diceValue;
     };
     updateDice();
 
     // Use the changed event to trigger the rerender whenever the value changes.
-    diceMap.on("valueChanged", updateDice);
+    diceString.on("sequenceDelta", updateDice);
 }
